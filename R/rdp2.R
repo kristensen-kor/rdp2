69ccef5084ffa13ec34b24c8318a1dd99fe699be
kristensen-kor <kristensen666@gmail.com>
2024-06-24 20:50:22 +0300

    removed has() from main file

--- R/rdp2.R
+++ R/rdp2.R
@@ -245,79 +245,8 @@ DS$set("public", "remove_labels", function(vars, ...) {
 })
 
 
-#' @export
-#'
-has = function(var, ...) {
-	values = c(...)
 
-	if (length(values) == 1) {
-		has1(var, values)
-	} else {
-		has_mult(var, values)
-	}
-}
-
-has1 = function(...) UseMethod("has1")
-
-has1.list = function(var, value) {
-	result = logical(length(var))
-
-	for (i in seq_along(var)) {
-		result[i] = any(var[[i]] == value)
-	}
-
-	result
-}
-
-# replace with C++ version? 3x faster
-
-# has1.list = function(var, value) {
-# 	has1_list_cpp(var, value)
-# }
-#
-# Rcpp::cppFunction("
-# LogicalVector has1_list_cpp(List var, int value) {
-#   int n = var.size();
-#   LogicalVector result(n, false);
-#
-#   for (int i = 0; i < n; i++) {
-#     IntegerVector vec = var[i];
-#     int len = vec.size();
-#     for (int j = 0; j < len; j++) {
-#       if (vec[j] == value) {
-#         result[i] = true;
-#         break;
-#       }
-#     }
-#   }
-#
-#   return result;
-# }
-# ")
-
-has1.default = function(var, value) {
-	xs = var == value
-	xs[is.na(xs)] = F
-	xs
-}
-
-has_mult = function(...) UseMethod("has_mult")
-
-has_mult.list = function(var, values) {
-	result = logical(length(var))
-
-	for (i in seq_along(var)) {
-		result[i] = any(var[[i]] %in% values)
-	}
-
-	result
-}
-
-has_mult.default = function(var, values) var %in% values
-
-
-
-# also between
+# add between
 
 #' @export
 bitcount = function(var, ...) {
@@ -330,7 +259,6 @@ bitcount = function(var, ...) {
 	}
 }
 
-# add non_valid values function
 
 
 #' @export
