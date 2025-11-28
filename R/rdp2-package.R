db4c9ba6e4b499e1323724871dba496e8109b1c1
kristensen-kor <kristensen666@gmail.com>
2025-11-28 13:46:24 +0200

    package reset
    Created package from scratch using devtools. Mostly formal changes to keep package modern.

--- R/rdp2-package.R
+++ R/rdp2-package.R
@@ -1,15 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' @import dplyr
-#' @import purrr
 #' @import haven
 #' @import openxlsx
-NULL
-
-## usethis namespace: start
+#' @import purrr
 #' @importFrom Rcpp sourceCpp
-## usethis namespace: end
-NULL
-
-## usethis namespace: start
 #' @useDynLib rdp2, .registration = TRUE
 ## usethis namespace: end
 NULL
